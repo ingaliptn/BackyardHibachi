8c88889538bdbb6e35037088a688400b298ee4da
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2024-01-29 15:33:33 +0200

    Add Project

---
+++ Controllers/MailController.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Mail;
+using BackyardHibachi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BackyardHibachi.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpPost]
+        public IActionResult ProcessForm(Contact cu, LogicModel l)
+        {
+            if (ModelState.IsValid)
+            {
+                string body = ConstructBodyFromContact(cu);
+                using (var client = new SmtpClient())
+                {
+                    client.Host = l.Smtp; // Your SMTP server (e.g., Gmail)
+                    client.Port = l.Port; // SMTP Port (Gmail uses 587)
+                    //client.EnableSsl = true;
+                    //client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential(l.Login, l.Password);
+                    var message = new MailMessage();
+                    message.From = new MailAddress(l.FromEmail, l.FromName); // Your email address
+                    string[] toEmails = l.ToEmail.Split(',');
+                    foreach (string email in toEmails)
+                    {
+                        message.To.Add(email.Trim());
+                    }
+                    message.Subject = l.Subject; // Email subject
+                    message.IsBodyHtml = true;
+                    message.Body = body;
+
+                    client.Send(message);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private string ConstructBodyFromContact(Contact cu)
+        {
+            return $"Name: {cu.Name}<br>" +
+            $"Phone: {cu.Phone}<br>" +
+            $"Email: {cu.Email}<br>" +
+            $"Company: {cu.Company}<br>" +
+            $"Message: {cu.Message}<br>";
+        }
+    }
+}
+
---
+++ Models/MailModel.cs
@@ -0,0 +1,27 @@
+﻿namespace BackyardHibachi.Models
+{
+    public class LogicModel
+    {
+        public string Smtp { get; set; } = "sp-vm.serverpipe.org";
+        public int Port { get; set; } = 587;
+        public bool IsEnableSsl { get; set; } = true;
+        public bool IsUseDefaultCredentials { get; set; }
+        public string Login { get; set; } = "relay@serverpipe.org";
+        public string Email { get; set; } = "relay@serverpipe.org";
+        public string Password { get; set; } = "Z326050r@";
+        public string FromName { get; set; } = "Backyard Hibachi";
+        public string FromEmail { get; set; } = "relay@serverpipe.org";
+        public string ToEmail { get; set; } = "nikitatkachuk6@gmail.com, hibachivegas247@gmail.com";
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Company { get; set; }
+        public string Message { get; set; }
+    }
+}
